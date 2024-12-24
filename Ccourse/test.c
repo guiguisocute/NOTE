442a0ab6388077a575f6f2ba09445099a7638916
guiguisocute_lap <g244629079@gmail.com>
2024-12-24 10:44:36 +0800

    push shit

--- Ccourse/test.c
+++ Ccourse/test.c
@@ -1,39 +1,10 @@
 #include <stdio.h>
 
-typedef struct {
-    int value;
-} Element;
-
-int binarySearch(Element nums[], int size, int target) {
-    int left = 0, right = size - 1;
-    
-    while (left <= right) {
-        int mid = (left + right) / 2;
-
-        if (nums[mid].value == target) {
-            return mid;
-        }
-        else if (nums[mid].value < target) {
-            left = mid + 1;
-        }
-        else { // nums[mid].value > target
-            right = mid - 1;
-        }
-    }
-    return -1;
-}
 
 int main() {
-    Element nums[] = {{1}, {3}, {5}, {7}, {9}};
-    int size = sizeof(nums) / sizeof(nums[0]);
-    int target = 5;
+    int a[5] = {1,2,3,4,5};
+    a[5] = 6;
 
-    int result = binarySearch(nums, size, target);
-    if (result != -1) {
-        printf("Element found at index: %d\n", result);
-    } else {
-        printf("Element not found.\n");
-    }
-
+    printf("%d",a[5]);
     return 0;
 }
